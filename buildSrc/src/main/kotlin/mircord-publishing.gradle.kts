e893510a3ccfe2f6818fbe2a96612f73901fc479
KiNgchev <ketone.git@gmail.com>
2024-10-02 15:35:17 +0300

    Update Publishing

--- buildSrc/src/main/kotlin/mircord-publishing.gradle.kts
+++ buildSrc/src/main/kotlin/mircord-publishing.gradle.kts
@@ -60,6 +60,15 @@ publishing {
                     password = getenv("NEXUS_PASSWORD")
                 }
             }
+
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/kiNgchev/mircord.kt")
+                credentials {
+                    username = getenv("GITHUB_ACTOR")
+                    password = getenv("GITHUB_TOKEN")
+                }
+            }
         }
     }
 }
